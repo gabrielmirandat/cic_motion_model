59d3a55ec5bf6cc62ef9cab09dbebc773d2ad718
thvlio <thulionoslen_@hotmail.com>
2017-06-27 15:18:05 -0300

    Tres linhas finais

--- MotionModel.cpp
+++ MotionModel.cpp
@@ -14,7 +14,10 @@ void MotionModel::sample_motion_model_odometry(Control u, Eigen::Vector3f state)
     float delta_rot_1 = atan2(u.getXt(1) - u.getXtm1(1),u.getXt(0) - u.getXtm1(0)) - u.getXtm1(2);
 
 
-
+    Eigen::Vector3d newstate;
+    newstate[0] = state[0] + delta_trans_corr * cos (state[2] + delta_rot_1_corr);
+    newstate[1] = state[1] + delta_trans_corr * sin (state[2] + delta_rot_1_corr);
+    newstate[2] = state[2] + delta_rot_1_corr + delta_rot_2_corr;
 
 }
 
